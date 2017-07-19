9da1e24ddd65f1e34b3d505daff40246f09fdad7
Walee Ahmed <walee.tausif@gmail.com>
2017-07-18 20:01:26 -0400

    added support for .env files & file cleanups

---
+++ pages/login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import DefaultLayout from '../layouts/default'
+
+const LoginPage = () =>
+  <DefaultLayout>
+    <h1>Login</h1>
+  </DefaultLayout>
+
+export default LoginPage
--- reducers/index.js
+++ reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux'
 import test from './TestReducer'
 
 export default combineReducers({
-  test  
+  test
 })
